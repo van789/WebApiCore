d99d6f8fbfb8bd6f347d01d7632606008472d10e
Vadym Kurinnyi <39626989+vadymkurinnyi@users.noreply.github.com>
2019-11-28 14:38:48 +0200

    Update UserHelpers.cs

--- Helpers/UserHelpers.cs
+++ Helpers/UserHelpers.cs
@@ -16,10 +16,18 @@ namespace WebApiCore.Helpers
             for (int i = 0; i < startInitCount; i++)
             {
                 var id = i;
+                
+                //дивсь комент до методу AddUserToCachAsync
+                // цю строку можна видалити
                 var newTask = Task.Run(() => cachedUsers.AddUserToCachAsync(id));
+                //коли метод AddUserToCachAsync буде повертати Task<bool>
+                //можна буде просто додати taskList.Add(cachedUsers.AddUserToCachAsync(id));
                 taskList.Add(newTask);
             }
-
+            
+           // в даний момент це WhenAll виконається одразу бо він опрацьовує синхронні методи у яких асинхронний код
+           // () => { // асинхроннийКод, запускає задачу але не чекає її, 
+            //з синхронного методу можна чекати .Result або .Wait(), якщо у тебе є десь async то більшість методів має бути async} 
             await Task.WhenAll(taskList);
         }
         
@@ -34,14 +42,17 @@ namespace WebApiCore.Helpers
         }
 
         #region Приватні методи
-
+            
+            
         private static string GetPath(int id)
         {
             string appDataPath = Environment.CurrentDirectory;
             string fileName = $"user{id}.json";
             return Path.Combine(appDataPath, fileName);
         }
-
+        
+        //Зберігати всіх юзерів у одному файлі у вигляді масиву юзерів
+        // Зробити цей метод потокобезпечним, зараз якщо 2 потока почне писати у файл, то буде пізда
         private static async Task SaveUserToFileAsync(User user)
         {
             var path = GetPath(user.Id);
@@ -57,11 +68,13 @@ namespace WebApiCore.Helpers
                 Console.WriteLine("ERROR Saving: " + e.Message);
             }
         }
-
+        
+        //Зробити цей метод async Task<bool>, якщо все пройшло успішно повернути true
         private static void AddUserToCachAsync(this List<User> cachedUsers, int newId = 0)
         {
             if (!cachedUsers.Any(x => x.Id == newId))
             {
+                //коли цей метод буде async, можна використати await GetUserFromFileAsync, і тоді .Result не потрібен
                 var newUser = GetUserFromFileAsync(newId).Result; //підчитали з файлу
                 if (newUser != null)
                 {
