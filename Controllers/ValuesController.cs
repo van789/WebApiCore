170060d851c23252b0dd9922d373c4b3d9904811
van789 <ivfialko@gmail.com>
2019-11-28 10:24:11 +0200

    Initial commit

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiCore.Filter;
+using WebApiCore.Models;
+using WebApiCore.Helpers;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace WebApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        static List<User> _cachedUsers= new List<User>();
+
+        public ValuesController()
+        {
+            _cachedUsers.InitUsersCachAsync().Wait();
+        }
+
+        // GET: api/Values
+        [HttpGet]
+        public IEnumerable<User> Get()
+        {
+            return _cachedUsers;
+        }
+
+        // GET: api/Values/5
+        [HttpGet("{id}", Name = "Get")]
+        [SimpleActionFilter]
+        public User Get(int id)
+        {
+            return _cachedUsers.Where(x => x.Id == id).FirstOrDefault();
+        }
+
+        // POST: api/Values
+        [HttpPost]
+        public async void Post([FromBody] User user)
+        {
+            _cachedUsers.SaveNewUserAsync(user); //не шарю нащо чекати
+        }
+
+        // PUT: api/Values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+
+        
+    }
+}
---
+++ Helpers/UserHelpers.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using WebApiCore.Models;
+
+namespace WebApiCore.Helpers
+{
+    public static class UserHelpers
+    {
+        public static async Task InitUsersCachAsync(this List<User> cachedUsers, int startInitCount = 10)
+        {
+            var taskList = new List<Task>();
+            for (int i = 0; i < startInitCount; i++)
+            {
+                var id = i;
+                var newTask = Task.Run(() => cachedUsers.AddUserToCachAsync(id));
+                taskList.Add(newTask);
+            }
+
+            await Task.WhenAll(taskList);
+        }
+        
+        public static async Task SaveNewUserAsync(this List<User> cachedUsers, User newUser)
+        {
+            SaveUserToFileAsync(newUser);
+
+            if (!cachedUsers.Any(x => x.Id == newUser.Id))
+            {
+                cachedUsers.Add(newUser);
+            }
+        }
+
+        #region Приватні методи
+
+        private static string GetPath(int id)
+        {
+            string appDataPath = Environment.CurrentDirectory;
+            string fileName = $"user{id}.json";
+            return Path.Combine(appDataPath, fileName);
+        }
+
+        private static async Task SaveUserToFileAsync(User user)
+        {
+            var path = GetPath(user.Id);
+            try
+            {
+                using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                {
+                    await JsonSerializer.SerializeAsync<User>(fs, user);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR Saving: " + e.Message);
+            }
+        }
+
+        private static void AddUserToCachAsync(this List<User> cachedUsers, int newId = 0)
+        {
+            if (!cachedUsers.Any(x => x.Id == newId))
+            {
+                var newUser = GetUserFromFileAsync(newId).Result; //підчитали з файлу
+                if (newUser != null)
+                {
+                    cachedUsers.Add(newUser);
+                }
+            }
+        }
+
+        private static async Task<User> GetUserFromFileAsync(int newId = 0)
+        {
+            var path = GetPath(newId);
+            try
+            {
+                if (File.Exists(path))
+                {
+                    using (FileStream fs = new FileStream(path, FileMode.OpenOrCreate))
+                    {
+                        return await JsonSerializer.DeserializeAsync<User>(fs);
+                    }
+                }                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR Reading: " + e.Message);
+            }
+            return null;
+        }
+
+        #endregion
+    }
+}
